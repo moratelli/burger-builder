c1962d35b7097962a4cb11e0c6dba87d305f9990
Pedro Moratelli <pedromoratelli@gmail.com>
2021-01-08 01:45:52 -0300

    Build burger dynamically

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -4,11 +4,26 @@ import classes from "./Burger.module.css";
 import Ingredient from "./Ingredient/Ingredient";
 
 const burger = (props) => {
+  let transformedIngredients = Object.keys(props.ingredients)
+    .map((igKey) => {
+      return [...Array(props.ingredients[igKey])].map((_, i) => {
+        return <Ingredient key={igKey + i} type={igKey} />;
+      });
+    })
+    .reduce((arr, el) => {
+      return arr.concat(el);
+    }, []);
+
+  console.log(transformedIngredients);
+
+  if (transformedIngredients.length === 0) {
+    transformedIngredients = <p>Please start adding ingredients!</p>;
+  }
+
   return (
     <div className={classes.Burger}>
       <Ingredient type="bread-top" />
-      <Ingredient type="cheese" />
-      <Ingredient type="meat" />
+      {transformedIngredients}
       <Ingredient type="bread-bottom" />
     </div>
   );
